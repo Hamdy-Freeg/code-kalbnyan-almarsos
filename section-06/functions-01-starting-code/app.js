b2f03e8b785bba20ac32af6db8f657737f126c62
Hamdy-Freeg <93085428+Hamdy-Freeg@users.noreply.github.com>
2023-01-25 13:20:52 +0200

    Add files via upload

---
+++ section-06/functions-01-starting-code/app.js
@@ -0,0 +1,36 @@
+/* const startGameBtn = document.getElementById("start-game-btn");
+
+function startGame() {
+  console.log("Game is starting...");
+}
+
+// const person = {
+//   greet: function greet(name) {
+//     console.log("hello " + name);
+//   },
+// };
+
+// console.log(person.greet("hamdy"));
+
+console.dir(startGame);
+
+startGameBtn.addEventListener("click", startGame);
+ */
+
+const startGameBtn = document.getElementById("start-game-btn");
+
+const start = function () {
+  console.log("Game is starting...");
+};
+
+// const person = {
+//   greet: function greet(name) {
+//     console.log("hello " + name);
+//   },
+// };
+
+// console.log(person.greet("hamdy"));
+
+// console.dir(start);
+
+startGameBtn.addEventListener("click", start);
