71846a6aa73a0cc23828e139e30249049e79c31c
Hamdy-Freeg <93085428+Hamdy-Freeg@users.noreply.github.com>
2023-03-01 15:22:54 +0200

    Add files via upload

---
+++ javaScript Application/myCode/section-24/practice-places-01-starting-setup/src/SharePlace.js
@@ -0,0 +1,51 @@
+import { Modal } from "./UI/Modal";
+import { Map } from "./UI/Map";
+
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
+
+    locateUserBtn.addEventListener("click", this.locateUserHandler.bind(this));
+    addressForm.addEventListener("submit", this.findUserHandler.bind(this));
+  }
+
+  selectPlace(coordinates) {
+    if (this.map) {
+      this.map.render(coordinates);
+    } else {
+      this.map = new Map(coordinates);
+    }
+  }
+
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert("Cannot find  your location,kindly use a modern browser");
+      return;
+    }
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location - please wait!"
+    );
+    modal.show();
+    navigator.geolocation.getCurrentPosition(
+      (successResult) => {
+        modal.hide();
+        console.log(successResult);
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+        };
+        this.selectPlace(coordinates);
+      },
+      (error) => {
+        modal.hide();
+        console.log("Cannot find your location, enter your address manually");
+      }
+    );
+  }
+
+  findUserHandler() {}
+}
+
+new PlaceFinder();
